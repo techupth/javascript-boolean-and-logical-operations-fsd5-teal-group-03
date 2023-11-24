8fb5cb4041bc8087f59bcffe24e81a11a61b147d
Name <pobparis.bun@gmail.com>
2023-11-24 14:04:08 +0700

    exercise complete

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,5 @@
-let isOver18;
-let hasCriminalBlacklist;
+let isOver18 = true; // James มีอายุ 18 ปี
+let hasCriminalBlacklist = false; // James ไม่เคยมีประวัติอาชญากรรม
 
-let isAllow;
-
-// Start coding here
-
-console.log(isAllow);
+let isAllow = isOver18 && !hasCriminalBlacklist; // เช็คว่าเป็นคนที่มีสิทธิ์เข้าร่วมงานหรือไม่
+console.log(isAllow); // ตรวจสอบค่า isAllow ว่า James มีสิทธิ์เข้างานหรือไม่
--- ex-2.js
+++ ex-2.js
@@ -1,12 +1,16 @@
 // Exercise #2: Promotion Conditions
 
 // Start coding here
-let lastMonthPaidMoreThan4000;
-let isWeekday;
-let hasBoughtProductFromITCategory;
-let hasAttendedDiscountEvent;
-let isPlatinum;
+let lastMonthPaidMoreThan4000 = true; // มียอดสั่งซื้อเดือนที่แล้วมากกว่า 4000 บาท
+let isWeekday = true; // ไปซื้อของวันศุกร์
+let hasBoughtProductFromITCategory = false; // ไม่เคยซื้อของในหมวดหมู่ IT มาก่อน
+let hasAttendedDiscountEvent = true; // เคยเข้าร่วมกิจกรรมมหกรรมลดราคาครั้งที่ 2
+let isPlatinum = false; // เป็น Member ระดับ Gold
 
-let hasPromotion;
-
-console.log(hasPromotion);
+let hasPromotion =
+  (lastMonthPaidMoreThan4000 &&
+    isWeekday &&
+    !hasBoughtProductFromITCategory &&
+    !hasAttendedDiscountEvent) ||
+  isPlatinum;
+console.log(hasPromotion); // ตรวจสอบค่า hasPromotion ว่า John มีสิทธิได้รับโปรโมชั่นส่วนลดหรือไม่
